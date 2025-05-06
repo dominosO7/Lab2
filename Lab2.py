bd40b79fa4e7339f5cc546cc41656968121a4d45
dominos <dominoeij@gmail.com>
2025-05-06 15:44:40 +0800

    new stuff

--- Lab2.py
+++ Lab2.py
@@ -7,19 +7,23 @@ def calculate_bmi(height, weight):
 
 def classify_bmi(bmi):
     if(bmi>=30.0):
+        returnvalue = 0
         print("Obese")
     elif(bmi<30.0 and bmi>25.0):
+        returnvalue = 1
         print("Overweight")
     elif(bmi>=18.5 and bmi<=25.0):
+        returnvalue = -1
         print("Underweight")
     else:
         print("Impossible BMI")
 
-    return
+    return returnvalue
 
 def app():
     output = calculate_bmi(1.79,50.0)
-    classify_bmi(output)
+    returnvalue = classify_bmi(output)
+    print("return value =", returnvalue)
     return
 if __name__ == "__main__":
     app()
